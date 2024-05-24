fa65e7277a2cf61a100a13d74939260ead3d3048
brahimbej <beajouibrahim451@gmail.com>
2024-05-24 15:00:53 +0100

    commit finally

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { AjoutIncidentComponent } from './ajout-incident/ajout-incident.component';
+
 import { AjoutIncidentFormComponent } from './ajout-incident-form/ajout-incident-form.component';
 import { TableComponent } from './table/table.component';
 import { ListRapportComponent } from './list-rapport/list-rapport.component';
 import { AddRapportComponent } from './add-rapport/add-rapport.component';
 import { CrudUsersComponent } from './crud-users/crud-users.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { MailComponent } from './mail/mail.component';
+
 
 const routes: Routes = [
   {path:"login", component:LoginComponent},
-  {path:"mail", component:MailComponent},
+
   {path:"dashboard", component:TableComponent},
   {path:"listrapport", component:ListRapportComponent},
   {path:"ajout_incident_form", component:AjoutIncidentFormComponent},
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,33 +6,29 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
 import { TableComponent } from './table/table.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { AjoutIncidentComponent } from './ajout-incident/ajout-incident.component';
+
 import { AjoutIncidentFormComponent } from './ajout-incident-form/ajout-incident-form.component';
 import { ListRapportComponent } from './list-rapport/list-rapport.component';
 import { AddRapportComponent } from './add-rapport/add-rapport.component';
 import { CrudUsersComponent } from './crud-users/crud-users.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { MailComponent } from './mail/mail.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
-    NavbarComponent,
+
     TableComponent,
-    SidebarComponent,
-    AjoutIncidentComponent,
+
     AjoutIncidentFormComponent,
     ListRapportComponent,
     AddRapportComponent,
     CrudUsersComponent,
     NotFoundComponent,
-    MailComponent
+
   ],
   imports: [
     BrowserModule,
